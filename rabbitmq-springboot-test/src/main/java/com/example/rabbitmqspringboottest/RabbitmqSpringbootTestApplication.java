3cc484f8ea90007317d27337f1df7d2693cc2040
stars-coding <2715800477@qq.com>
2024-01-19 18:10:06 +0800

    测试-第二次提交

--- rabbitmq-springboot-test/src/main/java/com/example/rabbitmqspringboottest/RabbitmqSpringbootTestApplication.java
+++ rabbitmq-springboot-test/src/main/java/com/example/rabbitmqspringboottest/RabbitmqSpringbootTestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RabbitmqSpringbootTestApplication {
 
     public static void main(String[] args) {
+        System.out.println("测试测试");
         SpringApplication.run(RabbitmqSpringbootTestApplication.class, args);
     }
 }
